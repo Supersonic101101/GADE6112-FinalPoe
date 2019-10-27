5512465c2ede859d8ba7120ab68f0727c2bea403
Supersonic101101 <55442156+Supersonic101101@users.noreply.github.com>
2019-10-27 14:01:10 +0200

    Updated

---
+++ GADE6112_Final_POE/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+    public float lookRadius = 10f;
+    Transform target;
+    Transform target2;
+    NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = PlayerManager.instance.player.transform;
+        target2 = PlayerManager.instance.player2.transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(target.position, transform.position);
+        float distance2 = Vector3.Distance(target2.position, transform.position);
+        if (distance <= lookRadius)
+        {
+            agent.SetDestination(target.position);
+            agent.SetDestination(target2.position);
+        }
+    }
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
+    }
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Wizard Attacks!");
+    }
+}
---
+++ GADE6112_Final_POE/Assets/Scripts/npcBehaviour.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class npcBehaviour : MonoBehaviour
+{
+    public float speed;
+    public float moveRate;
+
+    public int dirX;
+    public int dirY;
+
+    private float moveCounter;
+
+    private new Rigidbody2D rigidbody2D { get { return GetComponent<Rigidbody2D>() ?? default(Rigidbody2D); } }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rigidbody2D)
+        {
+            if(moveCounter > moveRate)
+            {
+                ChangeDirection();
+                moveCounter = 0f;
+            }
+            Vector2 vel = new Vector2(dirX * speed, dirY * speed);
+
+            rigidbody2D.velocity = Vector2.Lerp(rigidbody2D.velocity, vel, Time.deltaTime * 10f);
+
+            moveCounter += Time.deltaTime;
+        }
+    }
+
+    private void ChangeDirection()
+    {
+        dirX = Random.Range(-1, 1);
+        dirY = Random.Range(-1, 1);
+    }
+}
